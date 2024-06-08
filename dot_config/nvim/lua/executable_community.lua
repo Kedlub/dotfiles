c166b5c1c3e979a1fed6ce4d9004548a0a8be7b2
Kedlub <jakubkyzr@gmail.com>
2024-06-08 13:19:45 +0200

    Neovim config

---
+++ dot_config/nvim/lua/executable_community.lua
@@ -0,0 +1,23 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.recipes.heirline-clock-statusline" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+}
