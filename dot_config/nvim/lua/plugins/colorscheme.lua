73cbd4d238fa4d79c6797141d6302b51ff28b1e6
Kedlub <jakubkyzr@gmail.com>
2026-02-20 20:22:31 +0100

    feat: switch from astronvim to lazyvim

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    opts = {
+      flavour = "macchiato",
+      auto_integrations = true,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
