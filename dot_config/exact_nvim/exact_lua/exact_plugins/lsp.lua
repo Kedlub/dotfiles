e6807fd7ad191ff1c9756d79d5b2598e4c370a35
Kedlub <jakubkyzr@gmail.com>
2026-03-11 17:59:12 +0100

    feat(nvim): add auto py venv plugin

--- dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
+++ dot_config/exact_nvim/exact_lua/exact_plugins/lsp.lua
@@ -7,33 +7,9 @@ return {
     end,
   },
   {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          before_init = function(_, config)
-            local util = require("lspconfig.util")
-
-            local function find_python(startpath)
-              return util.search_ancestors(startpath, function(path)
-                local python = util.path.join(path, ".venv", "bin", "python")
-                if util.path.exists(python) then
-                  return python
-                end
-              end)
-            end
-
-            local python = find_python(vim.fn.getcwd())
-
-            if python then
-              config.settings = config.settings or {}
-              config.settings.python = config.settings.python or {}
-
-              config.settings.python.pythonPath = python
-            end
-          end,
-        },
-      },
-    },
+    "jglasovic/venv-lsp.nvim",
+    config = function()
+      require("venv-lsp").setup()
+    end,
   },
 }
